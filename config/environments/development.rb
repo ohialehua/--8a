84e39eab40fe3252b3b7671eeb50dfa5e6531d2f
EC2 Default User <ec2-user@ip-172-31-79-219.ec2.internal>
2021-12-07 14:29:28 +0000

    FINISH

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
     port:                 587,
     address:              'smtp.gmail.com',
     domain:               'gmail.com',
-    user_name:            'kairyu.bonvivant@gmail.com',
-    password:             'xqtecouxfvdaajrt',
+    user_name:            'USER_ADRESS',
+    password:             'YOUR_PASSWORD',
     authentication:       'login',
     enable_starttls_auto: true
   }
